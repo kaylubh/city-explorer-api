c21b19ded1c87a9929c300fbd412d1c619cc88a2
Caleb Hemphill <calebhemp@gmail.com>
2023-12-05 18:56:10 -0500

    Add weather data

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const cors = require('cors');
 // express
 const express = require('express');
 const app = express();
+// data
+const weatherData = require('./data/weather.json');
 
+// port
 const PORT = process.env.PORT || 3001;
 
 app.listen(3001, () => {
@@ -21,6 +24,16 @@ app.listen(3001, () => {
 
 app.get('/', (request, response) => {
   response.send('Hello from the other side');
+});
+
+app.get('/weather', (request, response) => {
+  response.json(weatherData);
+});
+
+app.get('/weatherRequest', (request, response) => {
+  const searchQuery = request.query.searchQuery;
+  const lat = request.query.lat;
+  const lon = request.query.lon;
 });
 
 // error handling
